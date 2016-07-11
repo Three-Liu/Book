14ba0e57b94430132a9496d1d014d5f6433535e5
Sirui Liu <463.clever@gmail.com>
2016-07-11 15:47:05 +0800

    Basically complete CenterActivity(Not Connect to WelcomeActivity with intent) next is going to work on Toolbar

--- app/src/main/java/com/shawn/br/CenterActivity.java
+++ app/src/main/java/com/shawn/br/CenterActivity.java
@@ -2,12 +2,14 @@ package com.shawn.br;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class CenterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_center);
     }
 }
